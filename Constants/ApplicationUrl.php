c3951368d5c685204568a56f709450b272d76156
Iyare I. Diagboya <toolz.skillz@gmail.com>
2023-06-27 15:05:20 +0100

    refactor: updated path for check rrr status

--- Constants/ApplicationUrl.php
+++ Constants/ApplicationUrl.php
@@ -2,15 +2,13 @@
 
 class ApplicationUrl
 {
-
-    // https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit
-    public static $demoUrl = "https://remitademo.net";
+   public static $demoUrl = "https://remitademo.net";
 
     public static $liveUrl = "https://login.remita.net";
 
     public static $genRRRpath = "/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit";
 
-    public static $rrrStatusPath = "/remita/ecomm/";
+    public static $rrrStatusPath = "/remita/exapp/api/v1/send/api/echannelsvc/";
 }
 
 ?>
